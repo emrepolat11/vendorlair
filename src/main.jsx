f267135de425550f0f05c6b1d98f02ea1bfa30fb
emrepolat11 <emrepolat@gmail.com>
2026-08-16 09:19:31 +0200

    Load paid plan vendor limit

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app'
 import PreviewEnhancements from './previewEnhancements'
+import PlanLimits from './planLimits'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <PlanLimits />
     <App />
     <PreviewEnhancements />
   </React.StrictMode>
