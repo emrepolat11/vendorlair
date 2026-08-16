f48f42999cc32cec355b00b935bb65c72aeb37a5
emrepolat11 <emrepolat@gmail.com>
2026-08-16 12:59:42 +0200

    Fix homepage auth entry flow

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,11 @@ import PreviewEnhancements from './previewEnhancements'
 import PlanLimits from './planLimits'
 import MobileResponsive from './mobileResponsive'
 
+const authMode = new URLSearchParams(window.location.search).get('mode')
+if (authMode === 'login' || authMode === 'signup') {
+  localStorage.removeItem('vl_session')
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <PlanLimits />
