8f2760e7b5a82926b6118fbd5d2b365bd8cdf75e
emrepolat11 <emrepolat@gmail.com>
2026-08-16 08:46:59 +0200

    Load VendorLair preview enhancements

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app'
+import PreviewEnhancements from './previewEnhancements'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><App /></React.StrictMode>
+  <React.StrictMode>
+    <App />
+    <PreviewEnhancements />
+  </React.StrictMode>
 )
